19bef43eb26fb5d19d4e0a88d5a5fe574124bcb8
jinbiao-bianjie <zhangjinbiao9693@gmail.com>
2022-07-27 18:05:39 +0800

    fix button style

--- src/components/common/NftResetButtonComponent.vue
+++ src/components/common/NftResetButtonComponent.vue
@@ -28,7 +28,11 @@ export default {
 <style scoped lang="scss">
 ::v-deep.nft_reset_button_container {
   margin-top: 0.2rem;
-
+  margin-left: 0.1rem;
+  @media (max-width: 460px) {
+    margin-top: 0.08rem;
+    margin-left: 0;
+  }
   .reset_btn {
     background: #fff;
     border-color: $theme_c;
